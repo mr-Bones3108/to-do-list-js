c810e185149bf9f3bbc7aed544ec56ef8a858bdd
mr-Bones3108 <nikhil788p@gmail.com>
2023-12-12 10:42:47 +0530

    edit README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # to-do-list-js
 I will make a todo List form odin project 
+This app will be made from HTML , CSS and JS in Js I will use SOLID Principles  
--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-console.log("Namaste Bhai Jan")
-
-
-
-const contentDiv = document.querySelector("#content");
-const testH1 = document.createElement("h1");
-testH1.textContent= "Hello I will make a Todo List using Odin Project"
-contentDiv.appendChild(testH1)
+console.log("Hello World")
