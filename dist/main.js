950aa0b2f6997c332710d21435eeffab4ffd2caf
mr-Bones3108 <nikhil788p@gmail.com>
2023-12-14 12:01:16 +0530

    Update scripts and website elements

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
@@ -16,17 +15,8 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_UI__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/UI */ \"./src/modules/UI.js\");\n\r\n\r\ndocument.addEventListener(\"DOMContentLoaded\", _modules_UI__WEBPACK_IMPORTED_MODULE_0__[\"default\"].loadWebsite);\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/modules/TodoList.js":
-/*!*********************************!*\
-  !*** ./src/modules/TodoList.js ***!
-  \*********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ TodoList)\n/* harmony export */ });\nclass TodoList {\r\n    constructor() {\r\n      this.projects = [];\r\n    }\r\n    addProject(project) {\r\n      if (this.projects.indexOf(project) > 0) return;\r\n      this.projects.push(project);\r\n    }\r\n    removeProject(projectName) {\r\n      const project = this.projects.find(\r\n        (project) => project.name === projectName\r\n      );\r\n      this.projects.splice(this.projects.indexOf(project), 1);\r\n    }\r\n  \r\n    getProject(projectName) {\r\n      return this.projects.find((project) => project.name === projectName);\r\n    }\r\n  \r\n    getProjects() {\r\n      return this.projects;\r\n    }\r\n  }\n\n//# sourceURL=webpack://to-do-list/./src/modules/TodoList.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_UI__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/UI */ \"./src/modules/UI.js\");\n\r\n\r\ndocument.addEventListener(\"DOMContentLoaded\", _modules_UI__WEBPACK_IMPORTED_MODULE_0__[\"default\"].loadWebsite());\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
 
 /***/ }),
 
@@ -34,9 +24,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /*!***************************!*\
   !*** ./src/modules/UI.js ***!
   \***************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ UI)\n/* harmony export */ });\n/* harmony import */ var _TodoList_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TodoList.js */ \"./src/modules/TodoList.js\");\n\r\n\r\nclass UI {\r\n  static loadWebsite() {\r\n    this.initDefaultButtons();\r\n    //const todoList = Storage.getTodoList()\r\n    //todolist/projects foreach createProject(project)\r\n    //todolist/project/tasks foreach createTask(task)\r\n  }\r\n\r\n  static initDefaultButtons() {\r\n    const inboxProjectsButton = document.getElementById(\"button-index\");\r\n    const todayProjectsButton = document.getElementById(\"button-today\");\r\n    const weekProjectsButton = document.getElementById(\"button-this-week\");\r\n    const addProjectButton = document.getElementById(\"button-add-project\");\r\n    const addTaskButton = document.getElementById(\"button-add-task\");\r\n\r\n    inboxProjectsButton.addEventListener(\"click\", UI.openInboxProjects);\r\n    todayProjectsButton.addEventListener(\"click\", UI.openTodayProjects);\r\n    weekProjectsButton.addEventListener(\"click\", UI.openWeekProjects);\r\n    addTaskButton.addEventListener(\"click\", UI.addTask);\r\n    addProjectButton.addEventListener(\"click\", UI.addProject);\r\n  }\r\n\r\n  static openInboxProjects() {\r\n    //for other random tasks which can't be assign as a project\r\n    console.log(\"Inside openInboxProjects\")\r\n  }\r\n\r\n  static openTodayProjects() {\r\n    //get all today tasks\r\n  }\r\n\r\n  static openWeekProjects() {\r\n    //get all this week tasks\r\n  }\r\n\r\n  static addProject() {\r\n    //show popup\r\n    //cancel - hide popup\r\n    //add - create popup\r\n    //create element and addEventListener (handleProjectNutton)\r\n    //add to todolist\r\n    //add to UI\r\n    //save local\r\n    //hide popup\r\n  }\r\n\r\n  static addTask() {\r\n    //show popup\r\n    //cancel - hide popup\r\n    //create element and addEventListener (handleTaskButton)\r\n    //add to todolist/project\r\n    //add to UI\r\n    //save local\r\n    //hide popup\r\n  }\r\n\r\n  createProject(project) {\r\n    //creates an element in DOM\r\n  }\r\n\r\n  createTask(task) {\r\n    //creates an element in DOM\r\n  }\r\n\r\n  handleProjectButton() {\r\n    //name clicked -> openProject\r\n    //options clicked -> openProjectSettings\r\n  }\r\n\r\n  openProject() {\r\n    //update UI\r\n  }\r\n\r\n  openProjectSettings() {\r\n    //show popup\r\n    //rename clicked -> renameProject\r\n    //remove clicked -> removeProject\r\n    //hide popup\r\n  }\r\n\r\n  renameProject() {\r\n    //show popup (same size as setProject popup)\r\n    //cancel - hide popup\r\n    //update todo (setName)\r\n    //update UI (textContent)\r\n    //save local\r\n    //hide popup\r\n  }\r\n\r\n  removeProject() {\r\n    //remove from todolist\r\n    //remove from UI\r\n    //save local\r\n  }\r\n\r\n  handleTaskButton() {\r\n    //tick -> setTaskCompleted\r\n    //text -> renameTask\r\n    //date -> setTaskDate\r\n  }\r\n\r\n  setTaskCompleted() {\r\n    //set UI tick completed\r\n    //cross task out (setTimeout)\r\n    //removeTask\r\n  }\r\n\r\n  removeTask() {\r\n    //remove from todoist/project\r\n    //remove from UI\r\n    //save local\r\n  }\r\n\r\n  renameTask() {\r\n    //show popup\r\n    //cancel - hide popup\r\n    //update todolist/project (setName)\r\n    //update UI (textContent)\r\n    //save local\r\n    //hide popup\r\n  }\r\n\r\n  setTaskDate() {\r\n    //show popup\r\n    //cancel - hide popup\r\n    //update todoist/project (setDate)\r\n    //update UI (textContent)\r\n    //save local\r\n    //hide popup\r\n  }\r\n}\n\n//# sourceURL=webpack://to-do-list/./src/modules/UI.js?");
+eval("throw new Error(\"Module parse failed: Unexpected token (142:29)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n|     //cancel - hide popup\\n|     //update todolist/project (setName)\\n>     //update UI (textContent)\");\n\n//# sourceURL=webpack://to-do-list/./src/modules/UI.js?");
 
 /***/ })
 
@@ -67,23 +57,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,11 +1,10 @@
 import TodoList from "./TodoList.js";
+import Storage from "./Storage.js";
 
 export default class UI {
   static loadWebsite() {
     this.initDefaultButtons();
-    //const todoList = Storage.getTodoList()
-    //todolist/projects foreach createProject(project)
-    //todolist/project/tasks foreach createTask(task)
+    this.loadStorageContent(); //from local storage
   }
 
   static initDefaultButtons() {
@@ -13,29 +12,47 @@ export default class UI {
     const todayProjectsButton = document.getElementById("button-today");
     const weekProjectsButton = document.getElementById("button-this-week");
     const addProjectButton = document.getElementById("button-add-project");
+    const addProjectPopupButton = document.getElementBy(
+      "button-add-project-confirm"
+    );
+    const cancelProjectPopupButton = document.getElementBy(
+      "button-add-project-cancel"
+    );
     const addTaskButton = document.getElementById("button-add-task");
+    const addTaskPopupButton = document.getElementBy("button-add-task-confirm");
+    const cancelTaskPopupButton = document.getElementBy(
+      "button-add-task-cancel"
+    );
 
     inboxProjectsButton.addEventListener("click", UI.openInboxProjects);
     todayProjectsButton.addEventListener("click", UI.openTodayProjects);
     weekProjectsButton.addEventListener("click", UI.openWeekProjects);
-    addTaskButton.addEventListener("click", UI.addTask);
-    addProjectButton.addEventListener("click", UI.addProject);
+    addProjectButton.addEventListener("click", UI.openAddProjectPopup);
+    addProjectPopupButton.addEventListener("click", UI.addProject);
+    cancelProjectPopupButton.addEventListener("click", UI.closeAddProjectPopup);
+    addTaskButton.addEventListener("click", UI.openAddTaskPopup);
+    addTaskPopupButton.addEventListener("click", UI.addTask);
+    cancelTaskPopupButton.addEventListener("click", UI.closeAddTaskPopup);
   }
+
+  // Default project button handlers
 
   static openInboxProjects() {
     //for other random tasks which can't be assign as a project
-    console.log("Inside openInboxProjects")
   }
-
   static openTodayProjects() {
     //get all today tasks
   }
-
   static openWeekProjects() {
     //get all this week tasks
   }
 
-  static addProject() {
+  // Add project button handlers
+
+  static openAddProjectPopup() {
+  }
+
+  static closeAddProjectPopup() {}
     //show popup
     //cancel - hide popup
     //add - create popup
@@ -43,10 +60,13 @@ export default class UI {
     //add to todolist
     //add to UI
     //save local
-    //hide popup
-  }
+    //hide popup}
 
-  static addTask() {
+  static addProject() {}
+
+  // Add task button handlers
+
+  static openAddTaskPopup() {
     //show popup
     //cancel - hide popup
     //create element and addEventListener (handleTaskButton)
@@ -56,30 +76,34 @@ export default class UI {
     //hide popup
   }
 
+  static closeAddTaskPopup() {}
+
+  static addTask() {}
+
+  // Creating DOM elements
+
   createProject(project) {
     //creates an element in DOM
   }
-
   createTask(task) {
     //creates an element in DOM
   }
+
+  // Project button handlers
 
   handleProjectButton() {
     //name clicked -> openProject
     //options clicked -> openProjectSettings
   }
-
   openProject() {
     //update UI
   }
-
   openProjectSettings() {
     //show popup
     //rename clicked -> renameProject
     //remove clicked -> removeProject
     //hide popup
   }
-
   renameProject() {
     //show popup (same size as setProject popup)
     //cancel - hide popup
@@ -88,46 +112,31 @@ export default class UI {
     //save local
     //hide popup
   }
-
   removeProject() {
     //remove from todolist
     //remove from UI
     //save local
   }
 
+  // Tasks button handlers
+
   handleTaskButton() {
     //tick -> setTaskCompleted
     //text -> renameTask
     //date -> setTaskDate
   }
-
   setTaskCompleted() {
     //set UI tick completed
     //cross task out (setTimeout)
     //removeTask
   }
-
   removeTask() {
     //remove from todoist/project
     //remove from UI
     //save local
   }
-
   renameTask() {
     //show popup
     //cancel - hide popup
     //update todolist/project (setName)
     //update UI (textContent)
-    //save local
-    //hide popup
-  }
-
-  setTaskDate() {
-    //show popup
-    //cancel - hide popup
-    //update todoist/project (setDate)
-    //update UI (textContent)
-    //save local
-    //hide popup
-  }
-}
