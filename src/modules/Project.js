919dca21acce1432ed31d7bb7a61d5f75043340c
mr-Bones3108 <nikhil788p@gmail.com>
2023-12-26 15:50:14 +0530

    Finish Today and Week tabs

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,47 +1,48 @@
-import { format } from "date-fns";
+import { toDate, isToday, isThisWeek, subDays } from "date-fns";
 
 export default class Project {
   constructor(name) {
     this.name = name;
     this.tasks = [];
   }
-
   setName(name) {
     this.name = name;
   }
-
   getName() {
     return this.name;
   }
-
   getTasks() {
     return this.tasks;
   }
-
   setTasks(tasks) {
     this.tasks = tasks;
   }
-
   getTask(taskName) {
     return this.tasks.find((task) => task.getName() === taskName);
   }
-
   contains(taskName) {
     return this.tasks.some((task) => task.getName() === taskName);
   }
-
   addTask(task) {
     if (this.tasks.indexOf(task) > 0) return;
     this.tasks.push(task);
   }
-
   deleteTask(taskName) {
     const task = this.tasks.find((task) => task.getName() === taskName);
     this.tasks.splice(this.tasks.indexOf(task), 1);
   }
 
   getTasksToday() {
-    const today = format(new Date(), "dd/MM/yyyy");
-    return this.tasks.filter((task) => task.getDate() === today);
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted());
+      return isToday(toDate(taskDate));
+    });
+  }
+
+  getTasksThisWeek() {
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted());
+      return isThisWeek(subDays(toDate(taskDate), 1));
+    });
   }
 }
--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -1,6 +1,5 @@
 import Project from "./Project";
 import Task from "./Task";
-
 export default class TodoList {
   constructor() {
     this.projects = [];
@@ -8,46 +7,54 @@ export default class TodoList {
     this.projects.push(new Project("Today"));
     this.projects.push(new Project("This week"));
   }
-
   getProjects() {
     return this.projects;
   }
-
   setProjects(projects) {
     this.projects = projects;
   }
-
   getProject(projectName) {
     return this.projects.find((project) => project.getName() === projectName);
   }
-
   contains(projectName) {
     return this.projects.some((project) => project.getName() === projectName);
   }
-
   addProject(project) {
     if (this.projects.indexOf(project) > 0) return;
     this.projects.push(project);
   }
-
   deleteProject(projectName) {
     const project = this.projects.find(
       (project) => project.getName() === projectName
     );
     this.projects.splice(this.projects.indexOf(project), 1);
   }
-
   updateTodayProject() {
     this.getProject("Today").tasks = [];
+    this.projects.forEach((project) => {
+      if (project.getName() === "Today" || project.getName() === "This week")
+        return;
+      const todayTasks = project.getTasksToday();
+      todayTasks.forEach((task) => {
+        const taskName = task.getName() + ` (${project.getName()})`;
+        this.getProject("Today").addTask(new Task(taskName, task.getDate()));
+      });
+    });
+  }
+
+  updateWeekProject() {
+    this.getProject("This week").tasks = [];
 
     this.projects.forEach((project) => {
       if (project.getName() === "Today" || project.getName() === "This week")
         return;
 
-      const todayTasks = project.getTasksToday();
-      todayTasks.forEach((task) => {
+      const weekTasks = project.getTasksThisWeek();
+      weekTasks.forEach((task) => {
         const taskName = task.getName() + ` (${project.getName()})`;
-        this.getProject("Today").addTask(new Task(taskName, task.getDate()));
+        this.getProject("This week").addTask(
+          new Task(taskName, task.getDate())
+        );
       });
     });
   }
